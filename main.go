d078f4b0e1a8bbec3b77cd995db79d059d716b34
Francis Yang <xyang42@ucsc.edu>
2017-12-02 18:48:01 -0800

    Changed db driver to beego/orm

--- main.go
+++ main.go
@@ -1,40 +1,86 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 	"database/sql"
-	"github.com/izayacity/LinkedIn/types"
 	_ "github.com/go-sql-driver/mysql"
-	"fmt"
+	"github.com/izayacity/LinkedIn/types"
+	"github.com/astaxie/beego/orm"
 )
 
+func init() {
+	// register model
+	orm.RegisterModel(new(types.User))
+	// set default database
+	err := orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:8889)/linkedin?charset=utf8")
+	checkErr(err)
+}
+
 func main() {
-	PORT := ":3000"
-	log.Print("Running server on "+ PORT)
+	// bind the port of the http server
+	PORT := ":5000"
+	fmt.Println("Running server on "+ PORT)
 	http.HandleFunc("/", showUsers)
-	log.Fatal(http.ListenAndServe(PORT, nil))
+
+	err := http.ListenAndServe(PORT, nil)
+	checkErr(err)
 }
 
 func checkErr(err error) {
 	if err != nil {
+		fmt.Println("ERR: %v\n", err)
 		panic(err)
 	}
 }
 
-func showUsers (w http.ResponseWriter, r *http.Request) {
-	// data models
-	var user types.User
+func printErrorID(id int64, err error) {
+	if err != nil {
+		fmt.Println("ID: %d, ERR: %v", id, err)
+	}
+}
 
+// test function connecting to the database and showing all the users using Orm pacakge
+func showUsers (w http.ResponseWriter, r *http.Request) {
+	o := orm.NewOrm()
+	user := types.User{Username:"Izaya", Password:"222", Email:"izayacity@gmail.com"}
+	fmt.Println("showusers")
+	// insert
+	//id, err := o.Insert(&user)
+	//printErrorID(id, err)
+
+	// update
+	//user.Username = "Izayacity"
+	//id, err = o.Update(&user)
+	//printErrorID(id, err)
+
+	// query
+	user = types.User{Username:"Izayacity"}
+	err := o.Read(&user, "username")
+
+	if err == orm.ErrNoRows {
+		fmt.Println("No result found.")
+	} else if err == orm.ErrMissPK {
+		fmt.Println("No primary key found.")
+	} else {
+		fmt.Println(user.Id, user.Username, user.Email)
+	}
+}
+
+// test function connecting to the database and showing all the users
+func dbTest (w http.ResponseWriter, r *http.Request) {
 	// db connection
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/linkedin?charset=utf8")
 	checkErr(err)
+	defer db.Close()
 
 	// a test query
 	getUserSQL := "SELECT * FROM  User"
 	rows, err := db.Query(getUserSQL)
 	checkErr(err)
 	defer rows.Close()
+
+	var user types.User
 
 	for rows.Next() {
 		user = types.User{}
