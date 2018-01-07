7ea6175e38f0f51f0226b8f78047047b12612364
Francis Yang <xyang42@ucsc.edu>
2018-01-08 00:40:36 +0800

    Added JWT validation for API requests

--- views/session.go
+++ views/session.go
@@ -10,6 +10,7 @@ import (
 	"time"
 	"strconv"
 	"io/ioutil"
+	"fmt"
 )
 
 const (
@@ -19,7 +20,8 @@ const (
 
 var SignKey []byte
 
-func initKeys(){
+// Load RSA key into SignKey
+func init(){
 	var err error
 
 	SignKey, err = ioutil.ReadFile(privKeyPath)
@@ -29,6 +31,7 @@ func initKeys(){
 	}
 }
 
+// Use for server error checking, panic when error is found
 func checkErr(err error, w http.ResponseWriter) {
 	if err != nil {
 		log.Print("Internal Server ERROR: ", err)
@@ -37,16 +40,52 @@ func checkErr(err error, w http.ResponseWriter) {
 	}
 }
 
+// Use for JSON serialization
+func JsonResponse(response interface{}, w http.ResponseWriter) {
+	data, err :=  json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
 // Used for each httpHandler to check if there is any active session
 func RequiresLogin(handler func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if !sessions.IsLoggedIn(r) {
+		if !isValidToken(r) && !sessions.IsLoggedIn(r) {
 			log.Print("Not logged in")
 			w.WriteHeader(401)
 			return
 		}
 		handler(w, r)
 	}
+}
+
+// Check if the JWT token in the request header is valid
+func isValidToken(r *http.Request) bool {
+	// read JWT token from the Authorization key of the request header
+	//tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkVGltZSI6MTUxNTMzNTg1NiwidXNlcm5hbWUiOiJ0ZXN0In0.3xyr8vczAD34ZTZH3eUPjRQGUpj164gUhnKfYglAji8"
+	tokenString := r.Header.Get("Authorization")
+	fmt.Println("tokenString: ", tokenString)
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return SignKey, nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println("Username: ", claims["username"])
+		fmt.Println("Created Time: ", claims["createdTime"])
+		return true
+	}
+	fmt.Println("Error of token: ", err)
+	return false
 }
 
 // Invalid the session information from the cookie store
@@ -62,19 +101,39 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 }
 
-func Login(w http.ResponseWriter, r *http.Request) {
-	// initialize session and user model
+// Write user data into session and cookie
+func initSession(w http.ResponseWriter, r *http.Request, u types.User) {
+	// Initialize session and user model
 	session, err := sessions.Store.Get(r, "LoginSession")
 	if err != nil {
 		log.Print("Fail to retrieve the session in login")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	session.Values["authenticated"] = "true"
+	session.Values["username"] = u.Username
+	session.Values["email"] = u.Email
+	session.Values["userid"] = u.Id
+
+	// Save the session. NOTE: has to be before any writing to the response
+	err = session.Save(r, w)
+	checkErr(err, w)
+
+	// Set Cookie on client for 3 months
+	expiration := time.Now().Add(90 * 24 * time.Hour)
+	cookieUsername := http.Cookie{Name: "username", Value: u.Username, Expires: expiration}
+	cookieId := http.Cookie{Name: "userid", Value: strconv.Itoa(u.Id), Expires: expiration}
+	http.SetCookie(w, &cookieUsername)
+	http.SetCookie(w, &cookieId)
+}
+
+// Login handler
+func Login(w http.ResponseWriter, r *http.Request) {
 	log.Print("POST  /v1/login/")
 	u := types.User{}
 
 	// Decode the received request body in JSON format
-	err = json.NewDecoder(r.Body).Decode(&u)
+	err := json.NewDecoder(r.Body).Decode(&u)
 	if err != nil {
 		log.Print("Bad Request JSON")
 		w.WriteHeader(400)
@@ -97,23 +156,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(401)		// not sure
 		return
 	}
-	session.Values["authenticated"] = "true"
-	session.Values["username"] = u.Username
-	session.Values["email"] = u.Email
-	session.Values["userid"] = u.Id
 
-	// Save the session. NOTE: has to be before any writing to the response
-	err = session.Save(r, w)
-	checkErr(err, w)
-
-	// Set Cookie on client for 3 months
-	expiration := time.Now().Add(90 * 24 * time.Hour)
-	cookieUsername := http.Cookie{Name: "username", Value: u.Username, Expires: expiration}
-	cookieId := http.Cookie{Name: "userid", Value: strconv.Itoa(u.Id), Expires: expiration}
-	http.SetCookie(w, &cookieUsername)
-	http.SetCookie(w, &cookieId)
-
-	initKeys()
+	// Important!
+	initSession(w, r, u)
 	// Create a new token object, specifying signing method and the claims
 	// you would like it to contain.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -134,17 +179,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	JsonResponse(response, w)
 }
 
-func JsonResponse(response interface{}, w http.ResponseWriter) {
-	json, err :=  json.Marshal(response)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(json)
-}
-
+// Register handler
 func Register(w http.ResponseWriter, r *http.Request) {
 	log.Print("POST request in /v1/register/")
 	u := types.User{}
